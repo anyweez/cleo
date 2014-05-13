99ed3838556a29e285ef42b3023339c5809ae900
luke-segars <luke@lukesegars.com>
2014-05-13 00:10:27 +0000

    A few files that were left out of the last commit.

---
+++ src/query/parse_test.go
@@ -0,0 +1,11 @@
+package query
+
+import "testing"
+import "io/ioutil"
+
+func TestParse1(t *testing.T) {
+	fp := io.Open("queries/thresh.lkg")
+	query_text := string( ioutil.ReadAll(fp) )
+
+	parse_tree := Parse(query_text)
+}
