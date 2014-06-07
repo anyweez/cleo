74714d32ea39c6b75a7e84c08b0628d16a6f0fd6
Luke Segars <luke@lukesegars.com>
2014-06-07 10:42:35 -0700

    First (mostly) working version of Angular frontend. Not connected to the backend yet.

---
+++ html/js/championlist.js
@@ -0,0 +1,10 @@
+// This will eventually come from the server.
+
+var championList = [
+	{
+		"id": 18,
+		"name": "Twitch",
+		"img": "http://ddragon.leagueoflegends.com/cdn/4.9.1/img/champion/Twitch.png",
+		"games": 88,
+	}
+];
